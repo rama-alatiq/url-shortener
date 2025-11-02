835a3acd9d171d2a98afac4e34f71b3048ff2d55
rama-alatiq <rama.alateeqq@gmail.com>
2025-11-02 09:44:59 +0300

    update main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.routers.url_shortner import router as url_shortner
-from fastapi.middleware.cors import CORSMiddleware
+# from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app:FastAPI):   
@@ -11,15 +11,15 @@ async def lifespan(app:FastAPI):
 app=FastAPI(lifespan=lifespan)
 app.include_router(url_shortner)
 
-origins = [
-    "http://127.0.0.1:8001",
-    "http://localhost:8001",
-]
+# origins = [
+#     "http://127.0.0.1:8001",
+#     "http://localhost:8001",
+# ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,           
-    allow_credentials=True,           
-    allow_methods=["*"],          
-    allow_headers=["*"],          
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,           
+#     allow_credentials=True,           
+#     allow_methods=["*"],          
+#     allow_headers=["*"],          
+# )
